41553b71a3473050149f2f2ff06c191f344f07ac
K0K0K7 <keerthik4444@gmail.com>
2020-01-02 19:01:52 +0530

    first checked

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { FeaturesComponent } from './features/features.component';
+import { PricingComponent } from './pricing/pricing.component';
+
+
+
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'features',component:FeaturesComponent},
+  {path:'pricing',component:PricingComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
