2f1bd2c70f4dd0e711cd7e0210619521b037c5f2
K0K0K7 <keerthik4444@gmail.com>
2020-01-02 11:31:12 +0530

    first checked

--- server.js
+++ server.js
@@ -7,4 +7,4 @@ app.use(express.static(__dirname+'/dist/Project'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/dist/Project/index.html')))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log('Example app listening on port ${port}!'))
